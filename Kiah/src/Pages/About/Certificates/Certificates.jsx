387109997652033d16c389df9ef84374b25b821f
kondjibokhare <kondajibokhare@gmail.com>
2024-10-15 18:29:17 +0530

    commit

--- Kiah/src/Pages/About/Certificates/Certificates.jsx
+++ Kiah/src/Pages/About/Certificates/Certificates.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import "./Certificates.css";
 import Hero from "../../../components/Hero/Hero";
+import Heading from "../../../components/Heading/Heading";
 
 const Certificates = () => {
   return (
     <div>
       <Hero title="certificates" />
-      Certificates
+      <Heading
+        subHead="Certificates We Achieved"
+        title="INDUSTRY LEVEL CERTIFICATES"
+      />
+      <div className="certificates">
+        <div className="certificate">
+          <div className="img">
+            <img src="https://www.kiahmetallurgical.com/admin/certificate_pdf/1616853170.pdf" alt="" />
+          </div>
+          <div className="info">
+            <div className="title">ISO 9001:2015</div>
+            <div className="para">
+              Organizations use the standard to demonstrate the ability to
+              consistently provide products and services that meet customer and
+              regulatory requirements.
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- Kiah/src/Pages/About/Make in India/MakeInIndia.jsx
+++ Kiah/src/Pages/About/Make in India/MakeInIndia.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
-import './MakeInIndia.css';
-import Hero from '../../../components/Hero/Hero';
+import React from "react";
+import "./MakeInIndia.css";
+import Hero from "../../../components/Hero/Hero";
 
 const MakeInIndia = () => {
   return (
     <div>
-       <Hero title="make in india" />
-      MakeInIndia</div>
-  )
-}
+      <Hero title="make in india" />
+      <div className="make-in-india">
+        <div className="image">
+          <img
+            src="https://www.kiahmetallurgical.com/images/resource/Make-In-India-Logo.png"
+            alt=""
+          />
+        </div>
+        <div className="info">
+          <p>
+            Indian Prime Minister, Sri. Narendra Modi, is endorsing Make In
+            India as one of the most important amendments in the Indian
+            Marketing History. This has given essential capability to the
+            foreign investors and international investment is at an all time
+            high in the Indian Market Sector today. Indian Manufacturers have
+            seen a potential growth leading to high quality Indian manufactured
+            items.
+          </p>
+          <p>
+            Taking his trend setting initiative up to our sleeve, we wear this
+            Make In India mark as a proud Industrialist. We assure that majority
+            of our supplied and exported products are manufactured locally with
+            the best of Indian originated raw materials.
+          </p>
+          <p>
+            We also understand that major industries and businesses require
+            specific imported raw materials and to accomplish their needs we
+            also import high quality material based on the client
+            specifications.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default MakeInIndia
+export default MakeInIndia;
